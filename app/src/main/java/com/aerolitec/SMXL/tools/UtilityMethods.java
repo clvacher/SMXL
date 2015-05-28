203e5807ef644dd6d24b4e200712915754eb3b31
clvacher <clement.vacher@hotmail.fr>
2015-05-28 15:51:37 +0200

    UtilityMethod (Commit de jerome)

---
+++ app/src/main/java/com/aerolitec/SMXL/tools/UtilityMethods.java
@@ -0,0 +1,35 @@
+package com.aerolitec.SMXL.tools;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by Jerome on 28/05/2015.
+ */
+public class UtilityMethods {
+
+    //Allows the ListView to adapt to its content test tet
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null)
+            return;
+
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.UNSPECIFIED);
+        int totalHeight = 0;
+        View view = null;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            view = listAdapter.getView(i, view, listView);
+            if (i == 0)
+                view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+            view.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += view.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
+    }
+}
